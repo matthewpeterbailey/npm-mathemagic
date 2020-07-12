3e7c3791230e16122a5e9d01c8aee6cb8e1e3d7d
Matthew Bailey <mpb1992@outlook.com>
2020-07-12 20:58:58 +0100

    Added jsconfig.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Core
 export { default as min } from "./core/min";
 export { default as max } from "./core/max";
+export { default as sum } from "./core/sum";
 
 // Geometry - Circle
 export { default as areaFromRadius } from "./geometry/circle/areaFromRadius";
@@ -11,3 +12,7 @@ export {
 	default as areaOfSquare,
 	default as square,
 } from "./geometry/square/areaOfSquare";
+
+// Stats
+export { default as mean } from "./stats/mean";
+export { default as mode } from "./stats/mode";
---
+++ src/stats/mean.js
@@ -0,0 +1,10 @@
+import sum from "../core/sum";
+const mean = (arr) => {
+	if (arr.length === 0) {
+		throw new Error("Mean requires an array with at least one element");
+	}
+
+	return sum(arr) / arr.length;
+};
+
+export default mean;
