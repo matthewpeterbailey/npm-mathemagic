437e5f157553fec4f4915d5a0c2893d2e78712b4
Matthew Bailey <mpb1992@outlook.com>
2020-10-25 15:54:46 +0000

    1.0.17

--- src/core/cross.ts
+++ src/core/cross.ts
@@ -5,17 +5,15 @@
  */
 
 export const cross = (vectA: number[], vectB: number[]): number[] => {
-    if (vectA.length !== 3 || vectB.length !== 3) {
-      throw new Error('cross() requires two array of numbers with three elements');
-    }
+  if (vectA.length !== 3 || vectB.length !== 3) {
+    throw new Error('cross() requires two array of numbers with three elements');
+  }
 
-    const crossProduct: number[] = [
-        vectA[1]*vectB[2]-vectA[2]*vectB[1],
-        vectA[2]*vectB[0]-vectA[0]*vectB[2],
-        vectA[0]*vectB[1]-vectA[1]*vectB[0]
-    ];
+  const crossProduct: number[] = [
+    vectA[1] * vectB[2] - vectA[2] * vectB[1],
+    vectA[2] * vectB[0] - vectA[0] * vectB[2],
+    vectA[0] * vectB[1] - vectA[1] * vectB[0],
+  ];
 
-    return crossProduct;
-
-  };
-  
+  return crossProduct;
+};
